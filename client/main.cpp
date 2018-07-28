97e6388925e5b09b3ec40a149e2d47b016e9e5ba
ktulhax <alex658737@yandex.ru>
2018-07-29 02:38:27 +0300

    add tcp server and client

--- client/main.cpp
+++ client/main.cpp
@@ -1,7 +1,55 @@
+#include <vector>
 #include <iostream>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 
-int main(int argc, char** argv)
+void run(uint16_t port)
 {
-    std::cout << "i'm a client" << std::endl;
-	return 0;
+    struct sockaddr_in serverAddr;
+    int clientSocket = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
+
+    if (clientSocket == -1)
+        throw std::runtime_error("socket creating error");
+
+    memset(&serverAddr, 0, sizeof (serverAddr));
+
+    serverAddr.sin_family = PF_INET;
+    serverAddr.sin_port = htons(port);
+    auto err = inet_pton(PF_INET, "127.0.0.1", &serverAddr.sin_addr);
+
+    if (err <= 0)
+    {
+        close(clientSocket);
+        throw std::runtime_error("ip address intialization error");
+    }
+
+    if (connect(clientSocket, (struct sockaddr*) &serverAddr, sizeof (serverAddr)) == -1) {
+        close(clientSocket);
+        throw std::runtime_error("connection error");
+    }
+
+    send(clientSocket, "hello server", strlen("hello server") + 1, 0);
+
+    shutdown(clientSocket, SHUT_RDWR);
+    close(clientSocket);
+}
+
+int main(void) {
+    try
+    {
+        run(12312);
+    }
+    catch(std::runtime_error& err)
+    {
+        std::cout << err.what()<< std::endl;
+        return EXIT_FAILURE;
+    }
+    std::cout << "ok"<< std::endl;
+    return 0;
 }
--- server/main.cpp
+++ server/main.cpp
@@ -1,7 +1,65 @@
+#include <vector>
 #include <iostream>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <sys/un.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <unistd.h>
+
+
+void run(uint16_t port)
+{
+    sockaddr_in serverAddr;
+    auto serverSocket = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
+
+    if (serverSocket == -1)
+        throw std::runtime_error("creating socket error");
+
+    memset(&serverAddr, 0, sizeof (serverAddr));
+    serverAddr.sin_family = PF_INET;
+    serverAddr.sin_port = htons(port);
+    serverAddr.sin_addr.s_addr = htonl(INADDR_ANY);
+
+    if (bind(serverSocket, (sockaddr*)&serverAddr, sizeof (serverAddr)) == -1)
+    {
+        close(serverSocket);
+        throw std::runtime_error("binding error");
+    }
+
+    if (listen(serverSocket, 10) == -1)
+    {
+        close(serverSocket);
+        throw std::runtime_error("listening error");
+    }
+
+    while (true)
+    {
+        int clientSocket = accept(serverSocket, 0, 0);
+        if (clientSocket < 0)
+        {
+            close(serverSocket);
+            throw std::runtime_error("accepting error");
+        }
+        std::vector<char> buf(524288);
+        auto rSize = recv(clientSocket, buf.data(), buf.size(), 0);
+        std::cout << "recieved message: " << buf.data() << std::endl;
+        shutdown(clientSocket, SHUT_RDWR);
+        close(clientSocket);
+    }
+}
 
 int main(int argc, char** argv)
 {
-	std::cout << "i'm a server" << std::endl;
-	return 0;
+    try
+    {
+        run(12312);
+    }
+    catch(std::runtime_error& err)
+    {
+        std::cout << err.what()<< std::endl;
+        return EXIT_FAILURE;
+    }
+    std::cout << "ok" << std::endl;
+    return 0;
 }
